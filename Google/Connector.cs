2ae33ef28046a90d3c718d9c3eb290a2d1b3c71c
yvan vander sanden <yvan@attr-x.net>
2018-05-22 23:00:50 +0200

    small update

---
+++ Google/Connector.cs
@@ -0,0 +1,76 @@
+﻿using Google.Apis.Admin.Directory.directory_v1;
+using Google.Apis.Admin.Directory.directory_v1.Data;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using YSchoolAPI;
+
+namespace Google
+{
+  public static class Connector
+  {
+    // only define scopes that you've given access for!
+    static private string[] scopes = {
+      DirectoryService.Scope.AdminDirectoryDomain,
+      DirectoryService.Scope.AdminDirectoryGroup,
+      DirectoryService.Scope.AdminDirectoryOrgunit,
+      DirectoryService.Scope.AdminDirectoryUser
+    };
+
+    static internal DirectoryService service;
+
+    static internal string Domain;
+
+    public static bool Init(string appName, string user, string domain, ILog log = null)
+    {
+      Domain = domain;
+      Error.log = log;
+
+      ServiceAccountCredential credential;
+
+      try
+      {
+        string secret = File.ReadAllText("client_secret.json");
+
+        dynamic values = JsonConvert.DeserializeObject(secret);
+        string key = values.private_key;
+        string ID = values.client_id;
+        string tokenURI = values.token_uri;
+
+
+        credential = new ServiceAccountCredential(
+            new ServiceAccountCredential.Initializer(ID, tokenURI)
+            {
+              User = user,
+              Scopes = scopes
+            }.FromPrivateKey(key));
+
+        service = new DirectoryService(new BaseClientService.Initializer()
+        {
+          HttpClientInitializer = credential,
+          ApplicationName = appName,
+        });
+
+      }
+      catch (Exception e)
+      {
+        Error.AddError(e.Message);
+        return false;
+      }
+
+      Error.AddMessage("Connection Suceeded");
+      return true;
+    }
+
+  }
+}
---
+++ Wisa/Connector.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wisa
+{
+  static public class Connector
+  {
+    private static WISA.TWISAAPICredentials credentials;
+    private static WISA.WisaAPIServiceService service;
+
+    static public void Init()
+    {
+      credentials = new WISA.TWISAAPICredentials();
+      credentials.Username = "";
+      credentials.Password = "";
+      credentials.Database = "";
+
+      service = new WISA.WisaAPIServiceService();
+      service.Url = "http://wenen.wisa-asp.net:8081/SOAP/";
+
+      
+    }
+
+    static public void GetClassList(string IDListCSV)
+    {
+      List<WISA.TWISAAPIParamValue> values = new List<WISA.TWISAAPIParamValue>();
+      values.Add(new WISA.TWISAAPIParamValue());
+      values.Last().Name = "IS_ID";
+      values.Last().Value = IDListCSV;
+      values.Add(new WISA.TWISAAPIParamValue());
+      values.Last().Name = "Werkdatum";
+      values.Last().Value = "22/05/2018";
+
+      try
+      {
+        var result = service.GetCSVData(credentials, "SMATestQ", values.ToArray(), true, ",", "");
+        var str = System.Text.Encoding.Default.GetString(result);
+        Debug.Write(str);
+      }
+      catch (Exception e)
+      {
+        Debug.WriteLine(e.Message);
+      }
+    }
+  }
+}
